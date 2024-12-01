9863cde71cf69f8eaec43e8e0936706d5eb3b83f
miriyamtrishni <114347445+miriyamtrishni@users.noreply.github.com>
2024-12-01 22:39:04 +0530

    add comment

--- android/app/src/main/kotlin/com/example/urbangreen/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/urbangreen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.urbangreen
 
+//Importing the necessary Flutter library to integrate with Android 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
